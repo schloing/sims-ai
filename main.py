335681102e77732e75061e33096029159743caa4
schloing <user.datagram.protocol@proton.me>
2023-07-10 18:41:04 +1000

    redefined traits

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import math
 from random import random, randint
 
+
 class GLOBALS:
     def __init__(self):
         self._broadcasts = []
         self._observers = []
-    
+
     @property
     def BROADCASTS(self):
         return self._broadcasts
@@ -19,6 +20,7 @@ class GLOBALS:
     def bind_to(self, callback):
         self._observers.append(callback)
 
+
 class Object:
     def __init__(self, broadcasts, name, advertisements, utility):
         self.name = name
@@ -30,14 +32,17 @@ class Object:
     def broadcast(self):
         self.broadcasts.BROADCASTS = self
 
+
 class Sim:
     def __init__(self, broadcasts, name, traits=None, stats=None):
         traits = traits or {
-            "EXTRAVER": random(),
-            "OPENNESS": random(),
-            "AGREEABL": random(),
-            "CONSCIEN": random(),
-            "NEUROTIC": random(),
+            "ANGER": random(),
+            "ACTIVE": random(),
+            "CHEER": random(),
+            "GENIUS": random(),
+            "CREATIVE": random(),
+            "LAZY": random(),
+            "SOCIAL": random(),
         }
         stats = stats or {
             "HUNGER": random(),
@@ -46,6 +51,7 @@ class Sim:
             "BLADDER": random(),
             "FUN": random(),
             "SOCIAL": random(),
+            "SATISF": random(),  # increased to promote behaviours that reflect traits
         }
         self.name = name
         self.traits = traits
@@ -56,12 +62,17 @@ class Sim:
 
     def weigh_stats(self):
         wants = {}
-        wants["HUNGER"]  = (1 / math.exp(self.stats["HUNGER"])) ** 4  # (\frac{1}{e^x})^4
-        wants["ENERGY"]  = (1 / math.exp(self.stats["ENERGY"])) ** 7  # (\frac{1}{e^x})^7
-        wants["HYGIENE"] = 1 - self.stats["HYGIENE"] ** (1 / 2)       # 1 - x^{\frac{1}{2}} (or sqrt(x))
-        wants["BLADDER"] = 1 - self.stats["BLADDER"] ** (1 / 3)       # 1 - x^{\frac{1}{3}} (or cube root)
-        wants["FUN"]     = 1 - self.stats["FUN"] ** (self.traits["EXTRAVER"] + self.traits["OPENNESS"])
-        wants["SOCIAL"]  = wants["FUN"]
+        wants["HUNGER"] = (1 / math.exp(self.stats["HUNGER"])) ** 4  # (\frac{1}{e^x})^4
+        wants["ENERGY"] = (1 / math.exp(self.stats["ENERGY"])) ** 7  # (\frac{1}{e^x})^7
+        wants["HYGIENE"] = 1 - self.stats["HYGIENE"] ** (
+            1 / 2
+        )  # 1 - x^{\frac{1}{2}} (or sqrt(x))
+        wants["BLADDER"] = 1 - self.stats["BLADDER"] ** (
+            1 / 3
+        )  # 1 - x^{\frac{1}{3}} (or cube root)
+        wants["FUN"] = 1 - self.stats["FUN"] ** self.traits["SOCIAL"]
+        wants["SOCIAL"] = wants["FUN"]
+        wants["SATISF"] = 1 / math.exp(self.stats["SATISF"])  # \frac{1}{e^x}
         return sorted(wants.items(), key=lambda x: x[1], reverse=True)
 
     def broadcast_listener(self, broadcasts):
@@ -83,17 +94,26 @@ class Sim:
         print(f"{'-' * 11}{'-' * len(self.name)}{'-' * 11}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     globals_obj = GLOBALS()
 
     jacob = Sim(globals_obj, "Jacob")
     jacob.print_char()
 
-    fridge = Object(globals_obj, "Fridge", {}, {
-        "HUNGER":   10,
-        "ENERGY":    5,
-        "BLADDER": -10,
-        "HYGIENE":  -5,
-    })
+    fridge = Object(
+        globals_obj,
+        "Fridge",
+        {  # TODO: weight these on basis of needs.
+            # a starving person should not lose "active" as much as a full person
+            "ACTIVE": -3,
+            "CHEER": 4,
+            "LAZY": 6,
+        },
+        {
+            "HUNGER": 10,
+            "ENERGY": 5,
+            "BLADDER": -10,
+            "HYGIENE": -5,
+        },
+    )
     fridge.broadcast()
-
